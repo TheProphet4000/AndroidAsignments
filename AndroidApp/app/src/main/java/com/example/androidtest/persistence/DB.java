419e3a259ce90a237a9bb285b924a934e5c65579
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-03-09 13:49:09 +0100

    Milepæl 7 færdig

---
+++ AndroidApp/app/src/main/java/com/example/androidtest/persistence/DB.java
@@ -0,0 +1,37 @@
+package com.example.androidtest.persistence;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "datakeeper";
+    private static final int DB_VERSION = 1;
+
+    public DB(Context context){
+        super(context,DB_NAME, null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { //laver tabels
+        String sqlDataName = "CREATE TABLE name (id PRIMARY KEY, AUROINCREMENT, name VARCHAR);";
+        db.execSQL(sqlDataName); //udføre sqlDataName
+    }
+
+    public boolean addName(String name){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        db.insert("names",null,contentValues);
+        db.close();
+        return true;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sqlDataName = "DROP TABLE IF EXISTS name"; //udføre ikke kommandoen hvis DB findes
+        onCreate(db);
+    }
+}
--- AndroidApp/app/src/main/java/com/example/androidtest/view/ViewModel.java
+++ AndroidApp/app/src/main/java/com/example/androidtest/view/ViewModel.java
@@ -6,16 +6,25 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.androidtest.R;
 import com.example.androidtest.model.Model;
+import com.example.androidtest.persistence.DB;
 
 public class ViewModel extends AppCompatActivity {
+
+    DB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DB(this); //wrapper DB
+        db.addName("Klaus Port"); //putter Klaus ind i DB
+
+        Toast.makeText(this, "Values saved to database", Toast.LENGTH_LONG).show();
     }
 
     @SuppressLint("SetTextI18n")
